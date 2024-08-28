3f26b94aa6e74a1a1e61f9f2ce0df582cac96faf
wahead1 <68811387+wahead1@users.noreply.github.com>
2024-08-28 18:46:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,4 +16,48 @@ document.addEventListener('DOMContentLoaded', (event) => {
     animateValue("visibility-stat", 0, 58, 2000);
     animateValue("trust-stat", 0, 71, 2000);
     animateValue("community-stat", 0, 20000, 2000);
+
+    const modal = document.getElementById("emailModal");
+    const btn = document.querySelector(".cta-button");
+    const closeButton = document.querySelector(".close-button");
+    const submitButton = document.getElementById("submitEmail");
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    closeButton.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    submitButton.onclick = function() {
+        const email = document.getElementById("emailInput").value;
+        if (email) {
+            fetch('http://localhost:3000/api/save-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+                modal.style.display = "none"; // Close the modal
+                alert("Thank you for Joining!");
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                alert("There was an error saving your email.");
+            });
+        } else {
+            alert("Please enter a valid email.");
+        }
+    }
 });
